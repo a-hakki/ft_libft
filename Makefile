3711b090857e4a45d54fe6ad1083eb0b353e5c3e
hatake <abdelazizhakki@gamil.com>
2024-10-23 09:08:47 -0400

    atoi commited succesfully

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 NAME = libft.a
-surce = ft_bzero.c ft_isascii.c ft_memcpy.c ft_split.c ft_strlen.c ft_isalnum.c ft_isdigit.c ft_memmove.c ft_strlcat.c ft_isalpha.c ft_isprint.c ft_memset.c ft_strlcpy.c ft_toupper.c ft_tolower.c ft_strchr.c ft_strrchr.c ft_strncmp.c ft_memchr.c ft_memcmp.c ft_strnstr.c ft_atoi.c ft_strdup.c ft_calloc.c ft_substr.c ft_strjoin.c ft_strtrim.c ft_itoa.c ft_strmapi.c ft_striteri.c ft_putchar_fd.c ft_putstr_fd.c ft_putendl_fd.c ft_putnbr_fd.c
+surce = ft_*.c
 object = $(surce:.c=.o)
 cc = gcc
 flags = -Wall -Wextra -Werror
@@ -19,4 +19,4 @@ test: all
 	@$(cc) $(flags) main.c -L. -lft -o test_pro	
 	@./test_pro
 
-re: fclean $(NAME)
+re: fclean all
--- ft_atoi.c
+++ ft_atoi.c
@@ -1,20 +1,23 @@
 #include "libft.h"
 
-int ft_atoi(const char *nptr)
+int	ft_atoi(const char *nptr)
 {
-    int i = 0;
-    int sing = 1;
-    int n = 0;
-    
-    while( (nptr[i] >= 9 && nptr[i] <= 13) || nptr[i] == 32)
-        i++;
-    if (nptr[i] == '+' || nptr[i] == '-')
-        if (nptr[i++] == '-')
-            sing = -1;
-    while (nptr[i] >= '0' && nptr[i] <= '9')
-    {
-        n = (n*10) + (nptr[i] - 48);
-        i++;
-    }
-    return (n * sing);
+	int	i;
+	int	sing;
+	int	n;
+
+	i = 0;
+	sing = 1;
+	n = 0;
+	while ((nptr[i] >= 9 && nptr[i] <= 13) || nptr[i] == 32)
+		i++;
+	if (nptr[i] == '+' || nptr[i] == '-')
+		if (nptr[i++] == '-')
+			sing = -1;
+	while (nptr[i] >= '0' && nptr[i] <= '9')
+	{
+		n = (n * 10) + (nptr[i] - 48);
+		i++;
+	}
+	return (n * sing);
 }
