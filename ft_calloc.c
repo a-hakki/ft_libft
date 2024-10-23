156572b660f9fcd957a8e02bdc19147112a3443b
hatake <abdelazizhakki@gamil.com>
2024-10-23 09:27:50 -0400

    calloc done

--- ft_calloc.c
+++ ft_calloc.c
@@ -1,19 +1,18 @@
 #include "libft.h"
 
-void *ft_calloc(size_t num, size_t size)
+void	*ft_calloc(size_t num, size_t size)
 {
-    size_t  i = 0;
-    char    *arr = malloc(num * size);
+	size_t	i;
+	char	*arr;
 
-    if (arr == NULL)
-    {
-        free(arr);
-        return NULL;
-    }
-    while (i < num)
-    {
-        arr[i] = 0;
-        i++;
-    }
-    return (arr);
+	arr = malloc(num * size);
+	i = 0;
+	if (arr == NULL)
+	{
+		free(arr);
+		return (NULL);
+	}
+	while (i < num)
+		arr[i++] = 0;
+	return (arr);
 }
